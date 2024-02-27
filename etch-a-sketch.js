ca884f305b65d653c55e72f202b56939aec74d9d
luluvillegas <lucrecia.villegas.gomez@gmail.com>
2024-02-27 11:04:28 -0500

    Added logic to change grid size
    Added option to change grid size and cleaning the grid every time the user changes
    the size.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,40 @@
-const container = document.querySelector(`.container`);
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement(`div`);
-  square.classList.add(`square`);
-  square.addEventListener(`mouseover`, () => {
-    square.classList.add(`draw`);
-    console.log(`yas`);
-  });
-  container.appendChild(square);
+function createGrid(numberOfSquares) {
+  let totalSquares = numberOfSquares * numberOfSquares;
+  console.log(
+    `You are seeing a ` + numberOfSquares + `*` + numberOfSquares + `grid`
+  );
+  for (let i = 0; i < totalSquares; i++) {
+    let square = document.createElement(`div`);
+    square.classList.add(`square`);
+    square.style.flexBasis = 100 / numberOfSquares + `%`;
+    square.addEventListener(`mouseover`, () => {
+      square.classList.add(`draw`);
+    });
+    container.appendChild(square);
+  }
 }
+
+function cleanGrid() {
+  container.innerHTML = ``;
+}
+
+let container = document.querySelector(`.container`);
+const defaultSquaresPerSide = 16;
+let totalSquares = defaultSquaresPerSide * defaultSquaresPerSide;
+
+createGrid(defaultSquaresPerSide);
+
+const startBtn = document.querySelector(`.start-btn`);
+startBtn.addEventListener(`click`, () => {
+  let squaresPerSide = prompt(
+    `How many squares per side? (between 1 and 100):`,
+    32
+  );
+  if (squaresPerSide < 1 || squaresPerSide > 100) {
+    alert(
+      `The number of squares per side must be bigger than 1 and smaller than 100!`
+    );
+  }
+  cleanGrid();
+  createGrid(squaresPerSide);
+});
