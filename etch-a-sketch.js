876fe0bcfcd91e300cee0627ab135088dcb677e2
luluvillegas <lucrecia.villegas.gomez@gmail.com>
2024-02-29 12:14:08 -0500

    Logic added to be able to chanche color modes
    Added logic to color button to be able to change from the current color back and forth.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,38 +1,57 @@
 function createGrid(numberOfSquares) {
   let totalSquares = numberOfSquares * numberOfSquares;
-  console.log(
-    `You are seeing a ` + numberOfSquares + `*` + numberOfSquares + `grid`
-  );
   for (let i = 0; i < totalSquares; i++) {
     let square = document.createElement(`div`);
     square.classList.add(`square`);
     square.style.flexBasis = 100 / numberOfSquares + `%`;
-    square.addEventListener(`mouseover`, () => {
-      square.classList.add(`draw`);
-    });
     container.appendChild(square);
   }
+}
+
+function setDrawColor(color) {
+  let squares = document.querySelectorAll(`.square`);
+  squares = Array.from(squares);
+  for (let i = 0; i < squares.length - 1; i++) {
+    squares[i].addEventListener(`mouseover`, () => {
+      if (color === BLACK_COLOR) {
+        squares[i].style.backgroundColor = BLACK_COLOR;
+      } else {
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        squares[
+          i
+        ].style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+      }
+    });
+  }
+  currentColor = color;
 }
 
 function cleanGrid() {
   container.innerHTML = ``;
 }
 
+const BLACK_COLOR = `rgb(67, 69, 68)`;
 let container = document.querySelector(`.container`);
-const defaultSquaresPerSide = 16;
-let totalSquares = defaultSquaresPerSide * defaultSquaresPerSide;
+let squaresPerSide = 16; //Default value
+let totalSquares = squaresPerSide * squaresPerSide;
+let currentColor = BLACK_COLOR; //Default color (black)
 
-createGrid(defaultSquaresPerSide);
+//Creates a default grid
+createGrid(squaresPerSide);
+setDrawColor(currentColor);
 
 const sizeBtn = document.querySelector(`.size-btn`);
 sizeBtn.addEventListener(`click`, () => {
-  let squaresPerSide = prompt(
+  squaresPerSide = prompt(
     `How many squares per side? (between 1 and 100):`,
     32
   );
   if (squaresPerSide > 1 && squaresPerSide < 100) {
     cleanGrid();
     createGrid(squaresPerSide);
+    setDrawColor(currentColor);
   } else {
     alert(
       `The number of squares per side must be bigger than 1 and smaller than 100!`
@@ -41,6 +60,10 @@ sizeBtn.addEventListener(`click`, () => {
 });
 
 const colotBtn = document.querySelector(`.color-btn`);
-sizeBtn.addEventListener(`click`, () => {
-  console.log(`change color`);
+colotBtn.addEventListener(`click`, () => {
+  if (currentColor === BLACK_COLOR) {
+    setDrawColor(`rainbow`);
+  } else if (currentColor === "rainbow") {
+    setDrawColor(BLACK_COLOR);
+  }
 });
